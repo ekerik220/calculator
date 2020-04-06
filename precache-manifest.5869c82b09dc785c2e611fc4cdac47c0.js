b6844781765609422cf200349e39de68bf9603f1
Evan K <ekerik220@gmail.com>
2020-04-06 15:56:09 +0900

    Updates

---
+++ precache-manifest.5869c82b09dc785c2e611fc4cdac47c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735aa4f53e3a2bf6ec664eb47c9ab955",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "7181fe75f017626eb463",
+    "url": "/calculator/static/css/main.68580d6c.chunk.css"
+  },
+  {
+    "revision": "7bc3614c14fb9e836cea",
+    "url": "/calculator/static/js/2.8851d343.chunk.js"
+  },
+  {
+    "revision": "fab1da7d8a32be3fe90e332131eacf28",
+    "url": "/calculator/static/js/2.8851d343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7181fe75f017626eb463",
+    "url": "/calculator/static/js/main.850a28be.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  }
+]);
